931cfe20bc5ae659e1aebf6d3dc02f25b95b018b
rafiuddinsadik-bkash <mohammad.sadique@bkash.com>
2022-12-06 13:06:41 +0600

    custom tcp 8080 added for web terraform

--- Modules/Networking/main.tf
+++ Modules/Networking/main.tf
@@ -72,6 +72,14 @@ resource "aws_security_group" "webserver_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description      = "Custom TCP"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "Web_SG"
   }
